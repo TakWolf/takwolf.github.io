63d896fd25b70907d9afcdb3fe32b615ae05ba23
TakWolf <takwolf@foxmail.com>
2022-04-01 17:00:11 +0800

    update

---
+++ js/theme.js
@@ -0,0 +1,16 @@
+/**
+ * 动态主题
+ */
+(function () {
+    function updateTheme() {
+        if (window.location.hash !== '') {
+            var themeString = window.location.hash.substr(1);
+            if (themeString === 'theme-light' || themeString === 'theme-dark') {
+                document.body.className = themeString;
+            }
+        }
+    }
+
+    window.addEventListener('hashchange', updateTheme, false);
+    updateTheme();
+})();
